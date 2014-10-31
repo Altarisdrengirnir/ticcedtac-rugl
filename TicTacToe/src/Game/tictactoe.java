bb0e1ed288515457336383a6ac9f2dae9a3e583b
Kari Mimisson <kari.mimisson@gmail.com>
2014-10-31 14:43:22 +0000

    All the classes. Next TODO code

--- TicTacToe/src/Game/tictactoe.java
+++ TicTacToe/src/Game/tictactoe.java
@@ -1,5 +1,20 @@
 package Game;
 
-public class tictactoe {
+/*
+	This is a project done by the Altaris.inc group in the
+	course Software engineering at the Reykjavik University in 
+	the fall of 2014.
+ */
+import java.awt.*;
 
+import javax.swing.*;
+
+
+public class tictactoe extends JFrame	
+{
+	private final int GRIDSIZE = 3; 
+	private static final boolean isXTurn = true;
+	
+	
+	 	
 }
